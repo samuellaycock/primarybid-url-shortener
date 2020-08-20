0b9a9cef04fe6e56b478fd32c8f22db866a75bd3
Samuel Laycock <saml@immjsystems.com>
2020-08-20 19:13:02 +0100

    fix: remove Vue productionTip mode setting due to failing build

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,10 +1,6 @@
 import Vue, { VNode } from "vue";
 import App from "./App.vue";
 
-if (process.env.NODE_ENV !== "development") {
-    Vue.config.productionTip = false;
-}
-
 new Vue({
     el: "#root",
     render(h): VNode {
