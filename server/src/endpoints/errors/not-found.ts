c0b252383333d916cd171612ce9af172bdedbede
Samuel Laycock <saml@immjsystems.com>
2020-08-20 08:53:56 +0100

    chore: update notFound endpoint handler and integration test code style

--- server/src/endpoints/errors/not-found.ts
+++ server/src/endpoints/errors/not-found.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "../../types"
 
-export default (req: Request, res: Response): void => {
+export default function notFound(req: Request, res: Response): void {
     res
         .status(StatusCodes.NOT_FOUND)
         .json({
