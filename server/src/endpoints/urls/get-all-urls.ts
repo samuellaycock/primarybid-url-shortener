f420de4fa5ff093890e442ff07ac888a3bbd861b
Samuel Laycock <saml@immjsystems.com>
2020-08-20 14:59:06 +0100

    refactor: remove getAllUrls API endpoint pagination support

--- server/src/endpoints/urls/get-all-urls.ts
+++ server/src/endpoints/urls/get-all-urls.ts
@@ -1,50 +1,23 @@
 import { Request, Response } from "express";
-import { Url } from "../../models";
+import { Url, UrlInterface } from "../../models";
 import { StatusCodes } from "../../enums";
+import { LINK_URL } from "../../env";
 
 export default async function getAllUrls(req: Request, res: Response): Promise<Response> {
-    const { query } = req;
-    const { page = 1, limit = 25 } = query;
-    const parsedPage = parseInt(page as string, 10);
-    const parsedLimit = parseInt(limit as string, 10);
-
-    if (Number.isNaN(parsedPage) || parsedPage < 1) {
-        return res.status(StatusCodes.BAD_REQUEST)
-            .json({
-                status: StatusCodes.BAD_REQUEST,
-                success: false,
-                error: "'page' must be a valid positive number"
-            });
-    }
-
-    if (Number.isNaN(parsedLimit) || parsedLimit < 1) {
-        return res.status(StatusCodes.BAD_REQUEST)
-            .json({
-                status: StatusCodes.BAD_REQUEST,
-                success: false,
-                error: "'limit' must be a valid positive number"
-            });
-    }
-
-    const [ urls, count ] = await Promise.all([
-        Url.find()
-            .skip((parsedPage - 1) * parsedLimit)
-            .limit(parsedLimit)
-            .lean(),
-        Url.countDocuments()
-    ]);
+    const urls = await Url.find()
+        .sort({ "_id": -1 })
+        .lean();
 
     return res.status(StatusCodes.SUCCESS)
         .json({
             status: StatusCodes.SUCCESS,
             success: true,
-            data: urls,
-            metadata: {
-                page: parsedPage,
-                prevPage: parsedPage - 1 || null,
-                nextPage: count > (parsedPage * parsedLimit) ? parsedPage + 1 : null,
-                items: urls.length,
-                totalItems: count
-            }
+            data: (urls as UrlInterface[]).map((url) => ({
+                code: url.code,
+                url: url.url,
+                link: `${LINK_URL}/${url.code}`,
+                createdAt: url.createdAt,
+                updatedAt: url.updatedAt
+            }))
         });
 }
--- tests/server/integration/urls/get-all-urls.test.ts
+++ tests/server/integration/urls/get-all-urls.test.ts
@@ -21,7 +21,7 @@ describe("integration::server/src/endpoints/urls/get-all-urls.ts", () => {
         });
 
         await Url.insertMany(
-            new Array(50).fill(null).map(() => ({ url: casual.url }))
+            new Array(10).fill(null).map(() => ({ url: casual.url }))
         );
     });
 
@@ -35,107 +35,12 @@ describe("integration::server/src/endpoints/urls/get-all-urls.ts", () => {
             .get("/api/v1/urls")
             .expect("Content-Type", /json/)
             .expect(200);
-        const { status, success, data, metadata } = res.body;
+        const { status, success, data } = res.body;
 
         // Response
         expect(status).toEqual(200);
         expect(success).toEqual(true);
         expect(data).toBeInstanceOf(Array);
-        expect(data.length).toEqual(25);
-        expect(metadata).toHaveProperty("page", 1);
-        expect(metadata).toHaveProperty("prevPage", null);
-        expect(metadata).toHaveProperty("nextPage", 2);
-        expect(metadata).toHaveProperty("items", 25);
-        expect(metadata).toHaveProperty("totalItems", 50);
-    });
-
-    test("it returns the correct response when selecting a page", async () => {
-        const res = await request(server.app)
-            .get("/api/v1/urls?page=2")
-            .expect("Content-Type", /json/)
-            .expect(200);
-        const { status, success, data, metadata } = res.body;
-
-        // Response
-        expect(status).toEqual(200);
-        expect(success).toEqual(true);
-        expect(data).toBeInstanceOf(Array);
-        expect(data.length).toEqual(25);
-        expect(metadata).toHaveProperty("page", 2);
-        expect(metadata).toHaveProperty("prevPage", 1);
-        expect(metadata).toHaveProperty("nextPage", null);
-        expect(metadata).toHaveProperty("items", 25);
-        expect(metadata).toHaveProperty("totalItems", 50);
-    });
-
-    test("it returns the correct response when selecting a limit", async () => {
-        const res = await request(server.app)
-            .get("/api/v1/urls?limit=5")
-            .expect("Content-Type", /json/)
-            .expect(200);
-        const { status, success, data, metadata } = res.body;
-
-        // Response
-        expect(status).toEqual(200);
-        expect(success).toEqual(true);
-        expect(data).toBeInstanceOf(Array);
-        expect(data.length).toEqual(5);
-        expect(metadata).toHaveProperty("page", 1);
-        expect(metadata).toHaveProperty("items", 5);
-        expect(metadata).toHaveProperty("prevPage", null);
-        expect(metadata).toHaveProperty("nextPage", 2);
-        expect(metadata).toHaveProperty("totalItems", 50);
-    });
-
-    test("it returns the correct error when selecting an invalid page", async () => {
-        const res = await request(server.app)
-            .get("/api/v1/urls?page=invalid_page")
-            .expect("Content-Type", /json/)
-            .expect(400);
-        const { status, success, error } = res.body;
-
-        // Response
-        expect(status).toEqual(400);
-        expect(success).toEqual(false);
-        expect(error).toEqual("'page' must be a valid positive number");
-    });
-
-    test("it returns the correct error when selecting a negative page", async () => {
-        const res = await request(server.app)
-            .get("/api/v1/urls?page=-1")
-            .expect("Content-Type", /json/)
-            .expect(400);
-        const { status, success, error } = res.body;
-
-        // Response
-        expect(status).toEqual(400);
-        expect(success).toEqual(false);
-        expect(error).toEqual("'page' must be a valid positive number");
-    });
-
-    test("it returns the correct error when selecting an invalid limit", async () => {
-        const res = await request(server.app)
-            .get("/api/v1/urls?limit=invalid_limit")
-            .expect("Content-Type", /json/)
-            .expect(400);
-        const { status, success, error } = res.body;
-
-        // Response
-        expect(status).toEqual(400);
-        expect(success).toEqual(false);
-        expect(error).toEqual("'limit' must be a valid positive number");
-    });
-
-    test("it returns the correct error when selecting a negative limit", async () => {
-        const res = await request(server.app)
-            .get("/api/v1/urls?limit=-1")
-            .expect("Content-Type", /json/)
-            .expect(400);
-        const { status, success, error } = res.body;
-
-        // Response
-        expect(status).toEqual(400);
-        expect(success).toEqual(false);
-        expect(error).toEqual("'limit' must be a valid positive number");
+        expect(data.length).toEqual(10);
     });
 });
